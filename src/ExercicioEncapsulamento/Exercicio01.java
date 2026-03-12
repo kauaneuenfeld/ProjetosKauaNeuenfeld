43d3946e5b740d9fe1bff560d3b0a2d343094fdb
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-03-12 17:45:22 -0300

    Update Exercicio01.java

--- src/ExercicioEncapsulamento/Exercicio01.java
+++ src/ExercicioEncapsulamento/Exercicio01.java
@@ -1,5 +1,35 @@
 package ExercicioEncapsulamento;
 
 public class Exercicio01 {
+	
+	private String nome;
+	private int idade;
+	
+	public Exercicio01(String nome, int idade) {
+		
+		this.nome =nome;
+		this.idade = idade;
+		
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	void Apresentar() {
+		System.out.println("Olá, meu nome é " + nome + " e tenho " + idade +" anos.");
+	}
+	
 }
